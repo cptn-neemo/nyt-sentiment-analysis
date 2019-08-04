010c3ea5f3a3292c6b21d3c92b2bddcb2f8d9cbb
Max Huddleston <maxhuddleston@gmail.com>
2019-08-04 13:12:08 -0400

    Get news from nyt api

--- serverless/get-news/index.js
+++ serverless/get-news/index.js
@@ -1,14 +1,33 @@
+const AWS = require('aws-sdk');
 const axios = require('axios');
 
-exports.handler = async (event) => {
+const s3 = new AWS.S3();
 
+exports.handler = async (event) => {
     const articles = 
-        await axios.get(`https://api.nytimes.com/svc/news/v3/content/all/all/13.json?api-key=${process.env.API_KEY}`);
-    const abstracts = articles.data.results.map(article => article.abstract);
-   
+        await axios.get(`https://api.nytimes.com/svc/news/v3/content/all/all/18.json?api-key=${process.env.API_KEY}`);
+
+
+    const params = {
+        Bucket: 'nyt-sentiment-analysis'
+    }
+
+    articles.data.results.forEach(async article => {
+        if (!article.url || !article.abstract)
+            return;
+
+        params.Key = article.title.split(' ').join('_');
+        params.Body = article.abstract;
+        try {
+            const putResult = await s3.putObject(params).promise();
+            console.log(putResult)
+        } catch (err) {
+            console.log("Error when putting");
+        }
+    });
+
 
     return {
-        statusCode: 200,
-        body: abstracts
+        statusCode: 200
     }
 }
