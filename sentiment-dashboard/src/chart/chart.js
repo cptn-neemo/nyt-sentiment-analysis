0f6177773fed131bf7965873630359d4b663784f
Max Huddleston <maxhuddleston@gmail.com>
2019-08-25 13:01:44 -0500

    Refactor get news lambda

--- sentiment-dashboard/src/chart/chart.js
+++ sentiment-dashboard/src/chart/chart.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import axios from 'axios'
-import { Line } from 'react-chartjs-2'
+import React from 'react';
+import axios from 'axios';
+import moment from 'moment';
+import { Line } from 'react-chartjs-2';
 
 const url = 'https://z0ljpmkx7h.execute-api.us-east-1.amazonaws.com/Prod/sentiment';
 
@@ -8,30 +9,66 @@ export default class ChartContainer extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = { chartData: {} }
     }
 
     componentDidMount() {
         axios.get(url)
             .then(response => {
                 const chartData = {
-                    labels: [],
+                    labels: [  ],
                     datasets: [
                         {
-                            data: []
+                            label: 'Sentiment',
+                            data: [ ],
+                            borderColor: [ '#F9D342' ],
+                            fill: false
                         }
-                    ]
+                    ],
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    min: -1,
+                                    max: 1 
+                                }
+                            }]
+                        }
+                    }
                 }
-                
-                response.data.body.Items.map(item => {
 
+                const dateBuckets = {}
+
+                response.data.body.Items.forEach(item => {
+                    const date = moment(item.created).format('M/D/YYYY');
+
+                    if (!dateBuckets[date])
+                        dateBuckets[date] = []
+                    dateBuckets[date].push(item.sentiment)
                 })
+
+                Object.keys(dateBuckets).sort().forEach(day => {
+                    const average = dateBuckets[day].reduce((a,b) => a + b, 0) / dateBuckets[day].length;
+
+                    chartData.labels.push(day);
+                    chartData.datasets[0].data.push(average)
+                })
+
+                console.log(chartData)
+
+                this.setState({ chartData })
             })
     }
 
     render() {
 
         return (
-            <div>Hello</div>
+            <div style={{ width: '66%', height: '50%' }}>
+                <Line
+                    data={this.state.chartData}
+                />
+            </div>
         );
     }
 }
--- serverless/get-news/index.js
+++ serverless/get-news/index.js
@@ -7,31 +7,32 @@ exports.handler = async (event) => {
     const articles = 
         await axios.get(`https://api.nytimes.com/svc/news/v3/content/all/all/18.json?api-key=${process.env.API_KEY}`);
 
-    //console.log(JSON.stringify(articles.data.results));
-
-    const params = {
-        Bucket: 'nyt-sentiment-analysis'
-    }
-
-    articles.data.results.forEach(async article => {
-        if (!article.url || !article.abstract)
+    const putPromises = articles.data.results.map(article => {
+        if (!article.title || !article.abstract)
             return;
         
-        params.Key = article.title.split(' ').join('_');
-        params.Body = article.abstract;
-
-//        console.log(article.title + ": " + article.abstract);
-
-        try {
-            const putResult = await s3.putObject(params).promise();
-            console.log("Successfully put object in s3.");
-        } catch (err) {
-            console.log("Error when putting");
+        const params = {
+            Bucket: 'nyt-sentiment-analysis',
+            Key:  article.title.split(' ').join('_'),
+            Body: article.abstract
         }
-    });
 
 
-    return {
-        statusCode: 200
+        return s3.putObject(params).promise();
+    })
+
+    try {
+        const results = await Promise.all(putPromises);
+
+        return {
+            statusCode: 200,
+            results
+        }
+    } catch (err) {
+        console.log('Error putting objects in S3');
+        return {
+            statusCode: 500,
+            message: 'Error putting objects in S3'
+        }
     }
 }
